28eab5677e2dc89effc3b156d0bf7b71ebf18d4f
Zeynep Sağlam <zzeynepssaglam@gmail.com>
2022-05-09 13:32:53 +0300

    fixed

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -18,6 +18,8 @@ export function healthCheck(name: string, heart: string): string {
   }
   return "Something looks wrong. Please have a detailed health check.";
 }
+//near call $CONTRACT healthCheck '{"name":"Zeynep","heart":"healthy"}' -accountId zzeyns.testnet
+//near call $CONTRACT healthCheck '{"name":"Zeynep","heart":"no good"}' -accountId zzeyns.testnet
 
 // Creates new flight.
 export function Launching_schedule(
@@ -38,10 +40,17 @@ export function Launching_schedule(
   );
 }
 
+//near call $CONTRACT Launching_schedule '{"text":"Moon and Back Mission","cost":"250000","capsul":7,"destination":"Moon","departure":"İstanbul","max_seat":7}' --accountId zzeyns.testnet
+//near call $CONTRACT Launching_schedule '{"text":"Survive Mission","cost":"78000000","capsul":1,"destination":"Saturn","departure":"Adana","max_seat":2}' --accountId zzeyns.testnet
+//near call $CONTRACT Launching_schedule '{"text":"Sun Trip","cost":"35000","capsul":10,"destination":"Sun","departure":"Juneau","max_seat":30}' --accountId zzeyns.testnet
+//near call $CONTRACT Launching_schedule '{"text":"20 minute space experince","cost":"200","capsul":100,"destination":"Exosphere","departure":"Berlin","max_seat":2000}' --accountId zzeyns.testnet
+//last one for buying seat.
+
 // Finds a flight by ID.
 export function specific_flight(id: u32): Flight {
   return Flight.show_specific_flight(id);
 }
+//near call $CONTRACT specific_flight '{"id":2354046042}' --accountId zzeyns.testnet
 
 // Shows all flights.
 export function Show_All(): Flight[] {
@@ -51,22 +60,26 @@ export function Show_All(): Flight[] {
   );
   return Flight.show_All();
 }
+//near call $CONTRACT Show_All '{}' --accountId zzeyns.testnet
 
 //Deletes a flight by ID.
 export function delete_specific(id: u32): Flight {
   return Flight.Specific_Flight(id);
 }
+//near call $CONTRACT delete_specific '{"id":1284343803}' --accountId zzeyns.testnet
 
 // Delete all flights.
 export function deleteall(): void {
   Flight.Delete_All();
   logging.log("Flights removed....");
 }
+//near call $CONTRACT deleteall '{}' --accountId zzeyns.testnet
 
 //Updates a flight by ID.
 export function Update(id: u32, updates: Flight): Flight {
   return Flight.updateinfos(id, updates);
 }
+//near call $CONTRACT Update '{"id":3111645745,"updates":{"text":"Moon and Back","cost":"560000","capsul":5,"destination":"Moon","departure":"İstanbul","max_seat":7}}' --accountId zzeyns.testnet
 
 //Buying seat.
 export function BuyingSeat(id: u32, passenger: AccountId): void {
@@ -76,3 +89,4 @@ export function BuyingSeat(id: u32, passenger: AccountId): void {
   logging.log(`Warning! ${context.attachedDeposit.toString()} yoktoNEAR sent`);
   ContractPromiseBatch.create(passenger).transfer(context.attachedDeposit);
 }
+//near call $CONTRACT BuyingSeat '{"id":2354046042,"passenger":"zzeyns.testnet"}' --accountId zzeyns.testnet
